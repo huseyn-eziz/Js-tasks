5ab2c285eaad90f8b09c9f2657d097a863efd822
huseyn-eziz <mastereziz@gmail.com>
2023-08-11 18:09:36 +0400

    t-03 finished

--- t-03/script.js
+++ t-03/script.js
@@ -1,3 +1,12 @@
-let en = prompt("Sahenin eni");
-let sahe = en * uzunluq;
-console.log("Duzbucaqlinin sahesi:" + sahe );
+let km = prompt("Km daxil edin");
+let mil = km * 0.6214;
+
+function miliyuvarla(mil) {
+    return Math.floor(mil * 100) / 100;
+}
+
+
+let yuvarlanmismil = miliyuvarla(mil);
+
+
+console.log(km + " km " + yuvarlanmismil + " mildir");
