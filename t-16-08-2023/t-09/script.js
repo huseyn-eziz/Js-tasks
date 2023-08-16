57ea1da50dcafd1b795140e5fe8a103f936ff09e
huseyn-eziz <mastereziz@gmail.com>
2023-08-16 22:45:54 +0400

    t-09 finished

---
+++ t-16-08-2023/t-09/script.js
@@ -0,0 +1,18 @@
+let e1 = +prompt(`Eded1 daxil edin:`)
+let e2 = +prompt(`Eded2 daxil edin:`)
+let e3 = +prompt(`Eded3 daxil edin:`)
+
+
+if (e1>e2 && e1>e3 ){
+
+    alert(`En boyuk eded: ` + e1)
+
+} else  if (e2>e1 && e2>e3 ){
+
+    alert(`En boyuk eded: ` + e2)
+
+} else {
+
+    alert(`En boyuk eded: ` + e3)
+
+}
