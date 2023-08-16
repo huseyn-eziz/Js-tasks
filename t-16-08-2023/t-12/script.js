86b38a10eaf74d8d97a2d7993ceef11dd7225934
huseyn-eziz <mastereziz@gmail.com>
2023-08-16 23:16:14 +0400

    t-12 finished

---
+++ t-16-08-2023/t-12/script.js
@@ -0,0 +1,15 @@
+let e3 = +prompt(`Ededi daxil edin`)
+
+if (e3 % 15 === 0) {
+
+    alert("fizzbuzz")
+
+} else if (e3 % 3 === 0) {
+
+    alert("fizz")
+
+} else if (e3 % 5 === 0) {
+
+    alert("buzz")
+
+} 
