b65e10abcabe66c9424c30e0ab3017d36743ac8f
huseyn-eziz <mastereziz@gmail.com>
2023-08-16 22:31:31 +0400

    t-07 finish

---
+++ t-16-08-2023/t-07/script.js
@@ -0,0 +1,16 @@
+let eded = +prompt(`Eded daxil edin:`)
+
+
+if (eded > 0){
+
+    alert(`Eded musbetdir`)
+
+} else if (eded < 0){
+
+    alert(`Eded mendifir`)
+
+} else if (eded === 0){
+
+    alert(`Eded 0-dir`)
+
+}
