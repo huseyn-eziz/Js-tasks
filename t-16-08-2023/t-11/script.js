6bbd528013b7cf2197f69f7a035d008bec09ad60
huseyn-eziz <mastereziz@gmail.com>
2023-08-16 22:57:02 +0400

    t-11 finish

---
+++ t-16-08-2023/t-11/script.js
@@ -0,0 +1,16 @@
+let e3 = prompt(`Nece daxil olmaq isteyirsiz? admin,user, yoxsa guest?`)
+
+
+if (e3 === "user") {
+
+    alert("xos geldin istifadeci")
+
+} else if (e3 === "admin") {
+
+    alert("xos geldin cenab admin")
+
+} else if (e3 === "guest") {
+
+    alert("xos geldin qonaq")
+
+} 
