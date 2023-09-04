87a34ab3523e72fc6e46afa46000a4b3605802b0
huseyn-eziz <mastereziz@gmail.com>
2023-09-04 12:38:05 +0400

    t-06 finish

---
+++ t-04-09-2023/t-06/script.js
@@ -0,0 +1,22 @@
+// 6.İstifadəçidən 1 ilə 99 arasında rəqəm daxil etməsini tələb edən və onu mətn şəklində konsola çıxaran numberToText funksiyası tərtib edin.
+
+function numberToText(reqem) {
+    const sozdekiteklikler = [
+        "Sıfır", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yeddi", "Sekkiz", "Doqquz"
+    ]
+    if (reqem < 10) {
+        console.log(sozdekiteklikler[reqem]);
+    } else {
+        const onluqlar = parseInt(reqem / 10);
+        const teklikler = reqem % 10;
+        const sozdekionluqlar = [
+            "", "On", "Yirmi", "Otuz", "Qirx", "Elli", "Altmış", "Yetmiş", "Seksen", "Doxsan"
+        ]
+        console.log(sozdekionluqlar[onluqlar] +' '+ sozdekiteklikler[teklikler].toLowerCase());
+
+    }
+}
+
+
+
+numberToText(97);
