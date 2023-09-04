5ecfa275a0746a62ad2a1b304b368eac81e36312
huseyn-eziz <mastereziz@gmail.com>
2023-09-04 12:44:45 +0400

    t-07 hazir

--- t-04-09-2023/t-07/script.js
+++ t-04-09-2023/t-07/script.js
@@ -1,44 +1,9 @@
-// 5.İstifadəçidən tarixi «YYYY.MM.DD» formatında daxil etməyi xahiş edin. Tarixin təsvirini «12 may 2019-cu il» formatında çıxarın. İstifadəçi səhv formatda dəyər daxil edərsə, «Yanlış dəyər daxil edilib» bildirişi göstərin.
+// 7.İstifadəçidən tam adı «AD SOYAD» formatında daxil etməyi xahiş edin. Tam adını konsola fərqli qaydada yəni «SOYAD AD» formatında çıxarın,
+
+const adsoyad = prompt('adı «AD SOYAD» formatında daxil')
+
+const ad = adsoyad.substring(0, adsoyad.indexOf(' '))
+const soyad = adsoyad.substring(adsoyad.indexOf(' '), adsoyad.length)
 
 
-
-// const tarix = prompt('Tarixi «YYYY.MM.DD» formatında daxil edin');
-function tarixyaz(tarix) {
-    const split = tarix.split(".")
-    const il = split[0]
-    const ay = split[1]
-    const gun = split[2]
-    if (tarix.length > 10 || il.length > 4 || ay.length != 2 || gun.length != 2 || ay > 12 || gun > 31) {
-        return ("Yanlış dəyər daxil edilib")
-    } else {
-        switch (ay) {
-            case "01":
-                return `${gun} yanvar ${il}-ci il`;
-            case "02":
-                return `${gun} fevral ${il}-ci il`;
-            case "03":
-                return `${gun} mart ${il}-ci il`;
-            case "04":
-                return `${gun} aprel ${il}-ci il`;
-            case "05":
-                return `${gun} may ${il}-ci il`;
-            case "06":
-                return `${gun} iyun ${il}-ci il`;
-            case "07":
-                return `${gun} iyul ${il}-ci il`;
-            case "08":
-                return `${gun} avqust ${il}-ci il`;
-            case "09":
-                return `${gun} sentyabr ${il}-ci il`;
-            case "10":
-                return `${gun} oktaybr ${il}-ci il`;
-            case "11":
-                return `${gun} noyabr ${il}-ci il`;
-            case "12":
-                return `${gun} dekabr ${il}-ci il`;
-        }
-    }
-}
-const tarix = prompt('Tarixi «YYYY.MM.DD» formatında daxil edin');
-console.log(tarixyaz(tarix));
-
+console.log(soyad + " " + ad);
--- yoxlama/script.js
+++ yoxlama/script.js
@@ -1,2 +1,9 @@
-const d = new Date();
-console.log(d.setFullYear(2023));
+// 7.İstifadəçidən tam adı «AD SOYAD» formatında daxil etməyi xahiş edin. Tam adını konsola fərqli qaydada yəni «SOYAD AD» formatında çıxarın,
+
+const adsoyad = prompt('adı «AD SOYAD» formatında daxil')
+
+const ad = adsoyad.substring(0, adsoyad.indexOf(' '))
+const soyad = adsoyad.substring(adsoyad.indexOf(' '), adsoyad.length)
+
+
+console.log(soyad + " " + ad);
