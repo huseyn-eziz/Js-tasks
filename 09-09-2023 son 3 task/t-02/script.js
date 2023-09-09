0e7dc9c21b76511c1004bf74961ec2103339140b
huseyn-eziz <mastereziz@gmail.com>
2023-09-09 13:40:24 +0400

    t-02 finish

--- 09-09-2023 son 3 task/t-02/script.js
+++ 09-09-2023 son 3 task/t-02/script.js
@@ -1,5 +1,12 @@
 //! 2. Funksiya yazin ki, massivde olan butun elementlerin hasilini ekrana cixarsin. 
 let massiv =[1,2,3,4,5]
 
-let massiv2 = massiv.map((massiv)=> massiv*2)
-console.log(massiv2);
+function hasili(massiv) {
+  let massiv2 = 1
+  for (let i = 0; i < massiv.length; i++) {
+    massiv2 = massiv[i] * 2
+    console.log(massiv2);
+  }
+}
+
+hasili(massiv)
