bb3e0852f4f45722152803a660e795ce05d1d660
huseyn-eziz <mastereziz@gmail.com>
2023-09-09 12:26:49 +0400

    map metod

---
+++ tapsiriqlar/mapmetodu.js
@@ -0,0 +1,4 @@
+const reqemler = [1, 2, 3, 4, 5];
+let kvadratlari = reqemler.map((kvadrat)=> kvadrat*kvadrat)
+console.log(kvadratlari);
+
--- yoxlama/script.js
+++ yoxlama/script.js
@@ -1,20 +1,6 @@
-let musteriler = [
-  [ 1 ,'Memmed','memmed@example.com' ],
-  [ 2 ,'Eli','eli@example.com' ],
-  [ 3 ,'Nermin','nermin@example.com' ]
-];
-
-let id = 2
-let yeniad ='Ali'
-let yeniemail = 'ali@example.com'
-let iditap = musteriler.find((idi) => idi[0] === id)
-
-
-if(iditap){
-  musteriler.ad = yeniad
-  musteriler.email = yeniemail
-  console.log(`${id} nomreli musterinin adi ${yeniad} ve emaili ${yeniemail} olaraq deyistirildi `);
-} else{
-  console.log(` bele mail yoxdur`);
-}
-
+const sayılar = [10, 20, 30, 40, 50];
+const hedefSayı = 30;
+const bulunanSayı = sayılar.findIndex(function(sayı) {
+  return sayı === hedefSayı;
+});
+console.log(bulunanSayı);
