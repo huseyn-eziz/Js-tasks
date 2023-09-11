40806482f91f46afca6c9222b22ed6ca7e7d827b
huseyn-eziz <mastereziz@gmail.com>
2023-09-11 09:18:02 +0400

    new objexercise added

--- tapsiriqlar/jschallangerobj.js
+++ tapsiriqlar/jschallangerobj.js
@@ -9,3 +9,15 @@
 
 // console.log(myFunction({  one: 1,  'prop-2': 2}));
 // console.log(myFunction({  'prop-2': 'two',  prop: 'test'}));
+
+//!Write a function that takes an object (a) and a string (b) as argument. Return true if the object has a property with key 'b'. Return false otherwise. Hint: test case 3 is a bit tricky because the value of property 'z' is undefined. But the property itself exists.
+
+// function myFunction(a,b) {
+
+//     console.log(a.hasOwnProperty(b)); 
+// }
+
+
+// myFunction({x:'a',y:'b',z:'c'},'a')
+// myFunction({a:1,b:2,c:3},'b')
+// myFunction({x:'a',y:'b',z:undefined},'z')
