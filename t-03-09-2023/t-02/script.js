e62df149becef7ae094bc948d7c60678d6a2e534
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-09-18 12:38:05 +0400

    proud of you 🤠

--- t-03-09-2023/t-02/script.js
+++ t-03-09-2023/t-02/script.js
@@ -1,6 +1,6 @@
 // 2. Write a JavaScript function to convert a string in abbreviated form. (Alion GreenHeart -> Alion G.) 
 
-
+// A: proud of you 🤠
 function qisalt(verilen) {
     return verilen.substring(0,verilen.indexOf(' ')) + ' G.' 
     
